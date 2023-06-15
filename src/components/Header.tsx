1a6f1f6450dca03dc2d31c20343ebbe34c5d23e4
hrithikscube <hrithik@scube.me>
2023-06-15 16:41:33 +0530

    menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 import tasteat from '../assets/images/tasteat.svg'
 import hero from '../assets/images/hero.png'
@@ -6,11 +6,11 @@ import hero from '../assets/images/hero.png'
 // import cart from '../assets/icons/cart.svg'
 import { menuItems, socials } from '../utils/helpers'
 import { Link, useLocation } from 'react-router-dom'
-
+import menu from '../assets/icons/menu.svg'
 
 const HeroSection = () => {
   return (
-    <div className='lg:w-[1378px] m-auto flex lg:flex-row flex-col  items-center lg:justify-between relative lg:py-20 '>
+    <div className='lg:w-[1378px] m-auto flex lg:flex-row flex-col items-center lg:justify-between relative lg:py-20 '>
 
       <div className='lg:w-[574px] flex flex-col gap-5 p-5'>
         <p className='lg:text-7xl md:text-3xl text-3xl font-cormorantBold text-white'>Welcome to
@@ -36,119 +36,146 @@ const SectionTitle = ({ text }: any) => {
 
 const Header = ({ error }: any) => {
 
+  const [toggle, setToggle] = useState(false)
+
   const { pathname } = useLocation()
 
   return (
-    <div className='bg-darkSlate w-full lg:py-10 py-5'>
+    <React.Fragment>
 
-      <div className='lg:flex hidden items-center lg:gap-10 justify-between  lg:w-[1378px] m-auto py-4'>
-        <Button variant="outlined" label="Call-987 654 321" />
-        <Link to="/sdas">
-          <img src={tasteat} alt="logo" className=']' />
-        </Link>
-        <div className='flex items-center gap-5'>
-          {/* <img src={cart} alt="cart" /> */}
-          <Button variant="contained" label="Reservation" />
+      {/* Menu Icon */}
+      <div onClick={() => setToggle(!toggle)} className='lg:hidden fixed top-[20px] right-[20px] cursor-pointer z-[99]'>
+        <div className='bg-[#323841] shadow-lg w-[60px] h-[60px] rounded-full flex justify-center items-center'>
+          <img src={menu} alt="" className='w-[32px] h-[32px]' />
         </div>
       </div>
 
-      <div className='lg:hidden flex justify-center items-center'>
-        <Link to="/sdas">
-          <img src={tasteat} alt="logo" className='scale-[0.9]' />
-        </Link>
-      </div>
-
-
-      <div className='border-y border-[#808080] p-5 lg:block hidden'>
-
-        <div className='flex items-center lg:w-[1378px] justify-between m-auto'>
-          <div className='flex items-center lg:gap-10 gap-5'>
-            {
-              menuItems.map((item: any) => (
-                <Link to={item?.link}>
-                  <p className='text-lg font-joseRegular cursor-pointer text-white'>{item.name}</p>
+      {
+        toggle && (
+          <div className='bg-[#323841] w-[250px] p-5 fixed right-[20px] top-[100px] z-[99]'>
+            <div className='flex flex-col justify-center items-center gap-5 p-5 w-full border border-white '>
+              {menuItems?.map((item: any) => (
+                <Link to={item?.link} onClick={() => setToggle(false)}>
+                  <p className='text-white font-joseRegular text-xl cursor-pointer'>{item?.name}</p>
                 </Link>
-              ))
-            }
+              ))}
+            </div>
           </div>
+        )
+      }
 
-          <div className='flex items-center lg:gap-5'>
-            {
-              socials.map((item: any) => (
-                <img src={item.icon} alt="" className='cursor-pointer' />
-              ))
-            }
+
+      <div className='bg-darkSlate w-full lg:py-10 py-5'>
+
+        <div className='lg:flex hidden items-center lg:gap-10 justify-between  lg:w-[1378px] m-auto py-4'>
+          <Button variant="outlined" label="Call-987 654 321" />
+          <Link to="/sdas">
+            <img src={tasteat} alt="logo" className=']' />
+          </Link>
+          <div className='flex items-center gap-5'>
+            {/* <img src={cart} alt="cart" /> */}
+            <Button variant="contained" label="Reservation" />
           </div>
         </div>
+
+        <div className='lg:hidden flex justify-center items-center'>
+          <Link to="/sdas">
+            <img src={tasteat} alt="logo" className='scale-[0.9]' />
+          </Link>
+        </div>
+
+
+        <div className='border-y border-[#808080] p-5 lg:block hidden'>
+
+          <div className='flex items-center lg:w-[1378px] justify-between m-auto'>
+            <div className='flex items-center lg:gap-10 gap-5'>
+              {
+                menuItems.map((item: any) => (
+                  <Link to={item?.link}>
+                    <p className='text-lg font-joseRegular cursor-pointer text-white'>{item.name}</p>
+                  </Link>
+                ))
+              }
+            </div>
+
+            <div className='flex items-center lg:gap-5'>
+              {
+                socials.map((item: any) => (
+                  <img src={item.icon} alt="" className='cursor-pointer' />
+                ))
+              }
+            </div>
+          </div>
+        </div>
+
+        <div className='flex justify-center items-center mt-10'>
+
+          {
+            pathname === '/' && (<HeroSection />)
+          }
+
+          {
+            pathname === '/about-us' && (
+              <SectionTitle text="About Us" />
+            )
+          }
+
+          {
+            pathname === '/our-menu' && (
+              <SectionTitle text="Our Menu" />
+            )
+          }
+
+          {
+            pathname === '/reserve' && (
+              <SectionTitle text="Booking Page" />
+            )
+          }
+
+          {
+            pathname === '/gallery' && (
+              <SectionTitle text="Gallery" />
+            )
+          }
+
+          {
+            pathname === '/blog' && (
+              <SectionTitle text="Blog" />
+            )
+          }
+
+          {
+            pathname === '/contact-us' && (
+              <SectionTitle text="Contact Us" />
+            )
+          }
+          {
+            pathname === '/changelog' && (
+              <SectionTitle text="Changelog" />
+            )
+          }
+          {
+            error && (
+              <SectionTitle text="404" />
+            )
+          }
+
+          {
+            pathname === '/faq' && (
+              <SectionTitle text="FAQ" />
+            )
+          }
+          {
+            pathname === '/protected' && (
+              <SectionTitle text="Password Protected" />
+            )
+          }
+
+
+        </div>
+
       </div>
-
-      <div className='flex justify-center items-center mt-10'>
-
-        {
-          pathname === '/' && (<HeroSection />)
-        }
-
-        {
-          pathname === '/about-us' && (
-            <SectionTitle text="About Us" />
-          )
-        }
-
-        {
-          pathname === '/our-menu' && (
-            <SectionTitle text="Our Menu" />
-          )
-        }
-
-        {
-          pathname === '/reserve' && (
-            <SectionTitle text="Booking Page" />
-          )
-        }
-
-        {
-          pathname === '/gallery' && (
-            <SectionTitle text="Gallery" />
-          )
-        }
-
-        {
-          pathname === '/blog' && (
-            <SectionTitle text="Blog" />
-          )
-        }
-
-        {
-          pathname === '/contact-us' && (
-            <SectionTitle text="Contact Us" />
-          )
-        }
-        {
-          pathname === '/changelog' && (
-            <SectionTitle text="Changelog" />
-          )
-        }
-        {
-          error && (
-            <SectionTitle text="404" />
-          )
-        }
-
-        {
-          pathname === '/faq' && (
-            <SectionTitle text="FAQ" />
-          )
-        }
-        {
-          pathname === '/protected' && (
-            <SectionTitle text="Password Protected" />
-          )
-        }
-
-
-      </div>
-
-    </div>
+    </React.Fragment>
   )
 }
 
